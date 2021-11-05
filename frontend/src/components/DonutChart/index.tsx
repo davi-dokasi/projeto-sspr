612feff1fe202c1604f72ceb7af317b684a17448
Davi Augusto <daviaugusto2509@gmail.com>
2021-11-05 15:37:41 -0300

    Integração no Donutchart/ Donutchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -12,19 +13,24 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    // FORMA ERRADA 
-    let chartData: ChartData = { labels: [], series: []};
+    // FORMA CORRETA/ CORRECT FORM
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/sales/amount-by-seller`) // Concatenação chique
-        .then(response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
 
-            chartData = { labels: myLabels, series: mySeries};
-            console.log(chartData);
-             })
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`) // Concatenação chique
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
+
+
+    // FORMA ERRADA/ WRONG FORM 
+    //let chartData: ChartData = { labels: [], series: []};
 
     /*
     const mockData = {
@@ -37,10 +43,10 @@ const DonutChart = () => {
             show: true
         }
     }
-   
+
     return (
         <Chart
-            options={{...options, labels: chartData.labels}}
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
